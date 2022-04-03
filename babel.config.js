9edc18d4aad8eb4e2db7e12d03af25a52f1030c9
Ranadeep <ranadeep.mobile@gmail.com>
2022-04-03 21:55:34 +0400

    update .gitignore; babel config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
+    '@parcel/babel-preset-env',
     ['@babel/preset-react', {runtime: 'automatic'}],
   ],
+  plugins: ["babel-plugin-styled-components"]
 };
